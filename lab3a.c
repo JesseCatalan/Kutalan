234c0cf43ed955514997fd94003c0e56139cadaa
Jesse Emmanuel Catalan <catalan@lnxsrv09.seas.ucla.edu>
2018-05-23 22:55:53 -0700

    Add lab3a.c

---
+++ lab3a.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <getopt.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Invalid invocation! ./lab3a [image]\n");
+        exit(1);
+    }
+}
