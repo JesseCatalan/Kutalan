ab1d81bca93c3a36eded4553668672c4d9c90927
Jesse Emmanuel Catalan <catalan@lnxsrv09.seas.ucla.edu>
2018-05-23 23:17:28 -0700

    lab3a.c parse arguments

--- lab3a.c
+++ lab3a.c
@@ -1,10 +1,14 @@
+#include "ex2_fs.h"
 #include <stdio.h>
-#include <getopt.h>
-#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Invalid invocation! ./lab3a [image]\n");
         exit(1);
     }
+
+    char *img_name = argv[1];
+    printf("%s\n", img_name);
 }
