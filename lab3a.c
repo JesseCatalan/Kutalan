38eeb0b85e7d22d7fd2f30a9d49f81dfc034aa7f
Jesse Emmanuel Catalan <catalan@lnxsrv07.seas.ucla.edu>
2018-05-24 00:20:14 -0700

    Initialize superblock struct

--- lab3a.c
+++ lab3a.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdint.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "ext2_fs.h"
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Invalid invocation! ./lab3a [image]\n");
+        fprintf(stderr, "Invalid invocation!\nUsage: ./lab3a [image]\n");
         exit(1);
     }
 
     char *img_name = argv[1];
-    printf("%s\n", img_name);
-    return 0;
+    int img_fd = open(img_name, O_RDONLY);
+
+    struct ext2_super_block sb;
+    pread(img_fd, &sb, sizeof(struct ext2_super_block), 1024);
 }
