0d2b75d3aeb5682832fe42f204208d4a522f8a6f
rkuchimpos <rkuchimpos@live.com>
2018-05-24 00:45:16 -0700

    Add superblock summary function; Update sources in README

--- lab3a.c
+++ lab3a.c
@@ -6,6 +6,38 @@
 #include <fcntl.h>
 #include "ext2_fs.h"
 
+/* Print summary of superblock:
+ * SUPERBLOCK
+ * total number of blocks (decimal)
+ * total number of i-nodes (decimal)
+ * block size (in bytes, decimal)
+ * i-node size (in bytes, decimal)
+ * blocks per group (decimal)
+ * i-nodes per group (decimal)
+ * first non-reserved i-node (decimal)
+ *
+ * Return 0 on success, -1 on error
+ */
+int print_superblock_summary(struct ext2_super_block *sb) {
+    if (sb->s_magic != EXT2_SUPER_MAGIC) {
+        return -1;
+    }
+
+    int num_blocks = sb->s_blocks_count;
+    int num_inodes = sb->s_inodes_count;
+    int block_size = EXT2_MIN_BLOCK_SIZE << sb->s_log_block_size;
+    int inode_size = sb->s_inode_size;
+    int blocks_per_group = sb->s_blocks_per_group;
+    int inodes_per_group = sb->s_inodes_per_group;
+    int first_nr_inode = sb->s_first_ino;
+
+    printf("SUPERBLOCK,%d,%d,%d,%d,%d,%d,%d\n",
+            num_blocks, num_inodes, block_size, inode_size,
+            blocks_per_group, inodes_per_group, first_nr_inode);
+
+    return 0;
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Invalid invocation!\nUsage: ./lab3a [image]\n");
